cb0d06b06d76f25bdf974ccfb7057a8718f64d51
Vidal Leonardo De Los Santos Rincon <niggbibi@hotmail.com>
2019-10-08 23:50:50 -0400

    socialmedia was added

--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -2,10 +2,13 @@ import React,{Fragment} from "react";
 
 import "../../assets/css/Footer.css";
 
+import SocialMedia from "../SocialMedia";
+
 const Footer = () => (
     <Fragment>
         <footer id="footer">
-            footer
+            <h5>niggbibi@hotmail.com</h5>
+            <SocialMedia/>
         </footer>
     </Fragment>
 );
