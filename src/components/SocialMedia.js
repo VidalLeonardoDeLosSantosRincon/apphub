565fb39e9435e720500a30e7f50ad1b4bebdbe5b
Vidal Leonardo De Los Santos Rincon <niggbibi@hotmail.com>
2019-10-08 23:55:24 -0400

    socialmediajs and socialmediacss were created and added

---
+++ src/components/SocialMedia.js
@@ -0,0 +1,31 @@
+import React,{Fragment} from "react";
+
+import "../assets/css/SocialMedia.css";
+
+import gitIcon from "../assets/images/github_media.png";
+import inIcon from "../assets/images/linkedin_media.png";
+import fbIcon from "../assets/images/facebook_media.png";
+import ytIcon from "../assets/images/youtube_media.png";
+
+const SocialMedia = (props) =>{
+    return(
+        <Fragment>
+            <div className="ctr-social_media">
+                <a target="_blank" href="https://github.com/VidalLeonardoDeLosSantosRincon">
+                    <img src={gitIcon} alt=""/>
+                </a>
+                <a target="_blank" href="https://www.linkedin.com/in/vidal-leonardo-de-los-santos-rincon-314644185/">
+                    <img id="linkedin" src={inIcon} alt=""/>
+                </a>
+                <a target="_blank" href="">
+                    <img src={fbIcon} alt=""/>
+                </a>
+                <a target="_blank" href="">
+                    <img src={ytIcon} alt=""/>
+                </a>
+            </div>
+        </Fragment>
+    );
+};
+
+export default SocialMedia;
